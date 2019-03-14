0d4f4b994c31029ab3f06e91f427dbb6ed0b1292
zyfarBektashov <bektasheov_z@auca.kg>
2019-03-14 21:46:12 +0600

    initial project

---
+++ AI-Hackathon-Cards/AI-Hackathon-Cards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AI-Hackathon-Cards
+//
+//  Created by Z on 14.03.2019.
+//  Copyright © 2019 Beta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
